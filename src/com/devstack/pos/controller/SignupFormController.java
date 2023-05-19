e1fd237a1c7e4a33b99b9f23ee8c035d73eb5582
charithanayana <124861158+charithanayana@users.noreply.github.com>
2023-05-19 22:51:28 +0530

    created sign up without encryption

--- src/com/devstack/pos/controller/SignupFormController.java
+++ src/com/devstack/pos/controller/SignupFormController.java
@@ -5,10 +5,15 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -16,6 +21,26 @@ public class SignupFormController {
     public JFXPasswordField txtPassword;
 
     public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                    "root","3267");
+            String sql = "insert into user VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2,txtPassword.getText());
+
+           if (preparedStatement.executeUpdate()>0) {
+               new Alert(Alert.AlertType.CONFIRMATION, "User Save!").show();
+           }else {
+               new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+            }
+
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
     public void btnAlreadyHaveAnAcountOnAction(ActionEvent actionEvent) throws IOException {
