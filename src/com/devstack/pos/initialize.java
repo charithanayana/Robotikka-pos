507cf5b7bb14745eb66c758689d1bf10d6611425
charithanayana <124861158+charithanayana@users.noreply.github.com>
2023-05-19 21:18:53 +0530

    created login & signup forms

--- src/com/devstack/pos/initialize.java
+++ src/com/devstack/pos/initialize.java
@@ -1,7 +1,11 @@
 package com.devstack.pos;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class initialize extends Application {
 
@@ -10,7 +14,10 @@ public class initialize extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))));
+        primaryStage.setTitle("Login");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
